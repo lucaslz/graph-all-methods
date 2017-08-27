597803ed8fe2e9f4d0f320238e9598d62d30411e
Lucas Lima <lucas.teolima@gmail.com>
2017-08-27 13:05:22 -0300

    Terminando de fazer os metodos para o cacheiro viajante.

--- src/graphtheoryjava/algorithms/ProblemasNP.java
+++ src/graphtheoryjava/algorithms/ProblemasNP.java
@@ -2,6 +2,7 @@ package graphtheoryjava.algorithms;
 
 import graphtheoryjava.util.Graph;
 import java.util.ArrayList;
+import java.util.Random;
 
 /**
  * Classe que implementa problemas do tipo NP= nao resolvivel
@@ -13,9 +14,16 @@ public class ProblemasNP {
     private Graph graph;
     public ArrayList u = new ArrayList();
     public ArrayList S = new ArrayList();
-
+    long tempoInicial;
+    long tempoFinal;
+    
     public ProblemasNP(Graph graph) {
         this.graph = graph;
+    }
+    
+    public void definirTempo() {
+        this.tempoInicial = System.currentTimeMillis();
+        this.tempoFinal = tempoInicial + 100000;        
     }
 
     public ArrayList neArestNeibhbor () {
@@ -23,24 +31,66 @@ public class ProblemasNP {
         this.u.add(naoVisitados.get(0));
         this.S.add(this.u.get(0));
         int v = 0;
-
+        
+        System.out.println(naoVisitados);
+        
         while (!naoVisitados.isEmpty()) {
             for (int i = 0; i < naoVisitados.size(); i++) {
-                for (int j = 0; j < naoVisitados.size(); j++) {
-                    if (this.u.get(0).equals(i)) {
-                        System.out.println(v+"===="+i);
-                        if (this.graph.adjMatrix[v][j] > this.graph.adjMatrix[i][j]) { 
-                            v = j;   
-                        }
-                    }
-                }
+                int c = (int) this.u.get(0);     
+                if ((i+1) < naoVisitados.size()) {
+                    if (this.graph.adjMatrix[c][i] < this.graph.adjMatrix[c][i++]) {
+                        v = i++;
+                    }    
+                }                
             }
-            
+            System.out.println(this.S);
             this.S.add(v);
             naoVisitados.remove(v);
-            this.u.add(0, v);
+            this.u.add(v);            
         }
-        this.S.add(this.S.get(0));
+        this.S.add(this.S.get(0));;
         return this.S;
+    }
+    
+    public ArrayList twooPt() {
+        
+        long inicio = System.currentTimeMillis();
+        Random randomGenerator = null;
+        ArrayList Slinha = new ArrayList();
+        
+        while(System.currentTimeMillis() - inicio <= 100000){
+            //Pegando indice aleatorio para i1 e i2
+            int i1 = randomGenerator.nextInt(this.S.size());
+            int i2 = randomGenerator.nextInt(this.S.size());
+            
+            if (i1 != i2) {
+                //Pegando arraylist S e passando para Slinha
+                Slinha = this.S;
+                
+                //Trocando S[i1] com S[i2]
+                int aux = (int) this.S.get(i1);
+                this.S.set(i1, this.S.get(i2));
+                this.S.set(i2, aux);
+                
+                if (true) {
+                    
+                }
+            }
+        }
+        return this.S;
+    }
+    
+    public int avalia() {
+        int custo = 0;
+        
+        for (int i = 0; i < this.S.size() - 1; i++) {
+            int u = (int) this.S.get(i);
+            int v = (int) this.S.get(i+1);
+            int w = this.graph.adjList.get(u).get(v).weight;
+            
+            custo = custo + w;
+        }
+        
+        return custo;
     }
 }
